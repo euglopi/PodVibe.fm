08b9e82e340a408aef3836c4b0fbebb997e40556
Hugo Melo <me@hugomelo.com>
2025-12-12 15:01:02 -0500

    rename app homepage, to then link to trending videos grid

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
+import { Route, Switch } from "wouter";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
 import LandingPage from "@/pages/LandingPage";
+// @ts-expect-error - JSX component without type declarations
+import SummarizerApp from "./SummarizerApp";
 
 function App() {
   return (
     <TooltipProvider>
       <Toaster />
-      <LandingPage />
+      <Switch>
+        <Route path="/" component={LandingPage} />
+        <Route path="/app" component={SummarizerApp} />
+      </Switch>
     </TooltipProvider>
   );
 }
